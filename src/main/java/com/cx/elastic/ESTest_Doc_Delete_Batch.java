6060df6518a353dd623c7e9d8f3b78202fe1d7d5
CarlChen88 <545663272@qq.com>
2022-05-22 14:05:34 +0800

    elasticsearch 批量文档文档操作

---
+++ src/main/java/com/cx/elastic/ESTest_Doc_Delete_Batch.java
@@ -0,0 +1,32 @@
+package com.cx.elastic;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+/**
+ * @author chenxiang
+ * @vesion 1.0
+ * @Description 批量插入文档
+ * @createTime 2022-05-22 08:45:45
+ */
+public class ESTest_Doc_Delete_Batch {
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.add(new DeleteRequest().index("user").id("1001"));
+        bulkRequest.add(new DeleteRequest().index("user").id("1002"));
+        bulkRequest.add(new DeleteRequest().index("user").id("1003"));
+        BulkResponse responses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println(responses.getIngestTook());
+        client.close();
+    }
+}
