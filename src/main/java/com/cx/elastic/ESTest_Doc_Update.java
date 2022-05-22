5178d3c88e66ccd7ccdc79da22b45f72ac3d6475
CarlChen88 <545663272@qq.com>
2022-05-22 09:31:16 +0800

    elasticsearch 文档操作

--- src/main/java/com/cx/elastic/ESTest_Doc_Update.java
+++ src/main/java/com/cx/elastic/ESTest_Doc_Update.java
@@ -6,11 +6,10 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.GetIndexRequest;
-import org.elasticsearch.client.indices.GetIndexResponse;
 import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.IOException;
@@ -18,21 +17,16 @@ import java.io.IOException;
 /**
  * @author chenxiang
  * @vesion 1.0
- * @Description 插入文档
+ * @Description 修改文档
  * @createTime 2022-05-22 08:45:45
  */
-public class ESTest_Doc_Insert {
+public class ESTest_Doc_Update {
     public static void main(String[] args) throws IOException {
         RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
-        IndexRequest indexRequest = new IndexRequest();
-        User user = new User();
-        user.setName("zhangsan");
-        user.setSex("男的");
-        user.setAge(30);
-        indexRequest.index("user").id("1001").source(JSONObject.toJSONString(user), XContentType.JSON);
-        IndexResponse response = client.index(indexRequest, RequestOptions.DEFAULT);
-        DocWriteResponse.Result result = response.getResult();
-        System.out.println(result);
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.index("user").id("1001");
+        updateRequest.doc(XContentType.JSON,"sex","女的");
+        client.update(updateRequest,RequestOptions.DEFAULT);
         client.close();
     }
 }
